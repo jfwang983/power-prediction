41d6c216ab4a43a0aca2849eb3e6c4eacd0b3ad7
jfwang983 <jfwang983@gmail.com>
2024-04-14 17:58:54 -0700

    fixed blocking error from hammer

--- scripts/init_rtl.sh
+++ scripts/init_rtl.sh
@@ -10,5 +10,6 @@ trap ctrl_c INT
 
 python modify_yml.py mvin_cache_hit_microbenchmark0
 cd ../power-mappings-chipyard/vlsi/
-make sim-rtl-debug BINARY=$BAREMETAL_BIN_DIR/mvin_cache_hit_microbenchmark0-baremetal LOADMEM=$BAREMETAL_BIN_DIR/mvin_cache_hit_microbenchmark0-baremetal
+make sim-rtl-debug BINARY=$BAREMETAL_BIN_DIR/mvin_cache_hit_microbenchmark0-baremetal LOADMEM=$BAREMETAL_BIN_DIR/mvin_cache_hit_microbenchmark0-baremetal &
+wait
 make power-rtl
