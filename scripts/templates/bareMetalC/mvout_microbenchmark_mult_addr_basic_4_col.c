e52d0b11af5f8be9c148f8c6f26cf5289dcd1837
Jonathan Wang <jfwang983@bwrcrdsl-5.EECS.Berkeley.EDU>
2025-03-14 16:03:54 -0700

    fixed mvout_mult_addr microbenchmarks

--- scripts/templates/bareMetalC/mvout_microbenchmark_mult_addr_basic_4_col.c
+++ scripts/templates/bareMetalC/mvout_microbenchmark_mult_addr_basic_4_col.c
@@ -12,8 +12,8 @@
 
 #define DIM 16
 #define ACC_ROWS 1024
-#define AccData_rows 16
-#define AccData_cols 64
+#define AccData_rows 128
+#define AccData_cols 16
 #define C_rows 16
 #define C_cols 4
 
@@ -33,9 +33,7 @@ int main() {
 
   // Matrix AccData, acc_t, AccData_rows, AccData_cols Setup
 
-	// Matrix AccData2, acc_t, AccData_rows, AccData_cols Setup
-
-  acc_t C[C_rows][C_cols];
+  elem_t C[C_rows][C_cols];
 
   uint32_t C_acc_base_addr = 1 << 31 | 0 << 30 | 1 << 29;
   uint32_t MAX_ACC_ADDR = C_acc_base_addr + ACC_ROWS;
@@ -58,15 +56,20 @@ int main() {
 
   // Config Setup
   gemmini_extended3_config_ld(AccData_cols * sizeof(acc_t), ACC_SCALE_IDENTITY, false, 0); // A Matrix
-  gemmini_config_st(C_cols * sizeof(acc_t));
+  gemmini_config_st(C_cols);
   // printf("gemmini_extended3_config_ld(%u, ACC_SCALE_IDENTITY, false, 0);\n", AccData_rows * sizeof(acc_t));
-  // printf("gemmini_config_st(%u);\n", C_cols * sizeof(acc_t));
+  // printf("gemmini_config_st(%u);\n", C_cols);
 
   // Move in matrices for accumulator initialization
-  for(int i = 0; i < ACC_ROWS; i += 128) {
-    gemmini_extended_mvin(AccData, C_acc_base_addr + i, 64, DIM);
-    gemmini_extended_mvin(AccData2, C_acc_base_addr + i + 64, 64, DIM);
+  for(int i = 0; i < ACC_ROWS; i += AccData_rows) {
+    for(int j = 0; j < AccData_rows; j += DIM) {
+      gemmini_extended_mvin(AccData[j], C_acc_addr, DIM, DIM);
+      C_acc_addr += DIM;
+    }
   }
+
+  C_acc_base_addr = 1 << 31;
+  C_acc_addr = C_acc_base_addr;
 
   // Main microbenchmark code
   for(int i = 0; i < iterations; i++) {
